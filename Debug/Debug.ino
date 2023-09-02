798ba0e8d08c352a22074ddf0f0f26d91fc9653f
Jon White <jonathon.white@stonybrook.edu>
2023-09-02 02:12:51 -0400

    Added ability to change hue

--- Debug/Debug.ino
+++ Debug/Debug.ino
@@ -9,8 +9,7 @@
 #define LED_COUNT 7
 
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
-double hue = (42.0/360.0)*65536.0;
-int hue_conv = (int)hue;
+int hue_conv = (int)((42.0/360.0)*65536.0);
 
 enum LED_STATE {
   RISE, 
@@ -30,10 +29,11 @@ private:
     LED_STATE ledState;
     int currentBrightness;
     int stepDelay; // Delay for each individual brightness step
+    int hueVal;
   
 public:
-    LedControl(int n, int brightness_max, int brightness_min, int delay_on, int delay_off, int delay_pulse, int start_brightness)
-      : ledNum(n), maxBrightness(brightness_max), minBrightness(brightness_min), ledState(RISE), currentBrightness(start_brightness) {
+    LedControl(int n, int brightness_max, int brightness_min, int delay_on, int delay_off, int delay_pulse, int start_brightness, int hue)
+      : ledNum(n), maxBrightness(brightness_max), minBrightness(brightness_min), ledState(RISE), currentBrightness(start_brightness), hueVal(hue) {
         
         int totalSteps = maxBrightness - minBrightness; // Calculate total number of brightness steps
         stepDelay = delay_pulse / totalSteps;           // Calculate delay for each step
@@ -79,20 +79,20 @@ public:
                 break;
         }
 
-        uint32_t color = strip.gamma32(strip.ColorHSV(hue_conv, 255, currentBrightness));
+        uint32_t color = strip.gamma32(strip.ColorHSV((hueVal/360.0)*65536.0, 255, currentBrightness));
         strip.setPixelColor(ledNum, color);
     }
 };
 
-#define NUM_LEDS 4
+#define NUM_LEDS 7
 LedControl leds[NUM_LEDS] = {
-    LedControl(0, 255, 0, 133, 400, 133, 0),
-    LedControl(1, 255, 0, 133, 267, 100, 0),
-    LedControl(2, 255, 0, 100, 167, 67, 0),
-    LedControl(3, 255, 50, 67, 233, 233, 0)
-    // LedControl(4, 255, 0, 100, 100, 10, 255),
-    // LedControl(5, 255, 0, 100, 100, 10, 255),
-    // LedControl(6, 255, 0, 100, 100, 10, 255)
+    LedControl(0, 255, 0, 133, 400, 133, 0, 0),
+    LedControl(1, 255, 0, 133, 267, 100, 0, 0),
+    LedControl(2, 255, 0, 100, 167, 67, 0, 42),
+    LedControl(3, 255, 50, 67, 233, 233, 0, 0),
+    LedControl(4, 255, 0, 100, 167, 67, 0, 42),
+    LedControl(5, 255, 0, 133, 267, 100, 0, 0),
+    LedControl(6, 255, 0, 133, 400, 133, 0, 0)
 };
 
 void setup() {
